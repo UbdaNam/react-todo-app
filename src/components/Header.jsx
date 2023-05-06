7cec2bd50400d8f761eae12efac9fbb2488e82a0
Abdurahim Miftah <abdugreat3@gmail.com>
2023-05-06 17:09:31 +0300

    Add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>todos</h1>
+    </header>
+  );
+};
+export default Header;
